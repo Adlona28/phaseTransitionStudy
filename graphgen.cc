901bcdfaaaa5a31312d47a590597b602cca5dcc2
adrian.lozano.navarro@est.fib.upc.edu <adrian.lozano.navarro@est.fib.upc.edu>
2019-05-10 21:46:27 +0200

    Commit de cosas

--- graphgen.cc
+++ graphgen.cc
@@ -11,66 +11,63 @@ const uint MAX_NODES = 1000;
 const uint MAX_WEIGHT = 100;
 
 //Undirected graphs generacion
-MATR genUniform(){
+MATR genUniform() {
     //int n = (rand() % MAX_NODES) + 50;
     int n = 100;
-    MATR graph (n,vector<int>(n));
+    MATR graph (n, vector<int>(n));
 
     for (int i = 0; i < n; i++) {
-        graph[n].resize(n, 0);
         graph[i][i] = -1;
     }
-    int m = rand() % ((n*(n-1))/2+1);
+    int m = rand() % ((n * (n - 1)) / 2 + 1);
 
-    while (m > 0){
+    while (m > 0) {
         int i = rand() % n;
         int j = rand() % n;
-        if(i != j and graph[i][j] == 0){
-             graph[i][j] = 1;
-             graph[j][i] = 1;
-             --m;
+        if (i != j and graph[i][j] == 0) {
+            graph[i][j] = 1;
+            graph[j][i] = 1;
+            --m;
         }
     }
     return graph;
 }
 
-MATR genRandomGraph(int n){
-    MATR graph (n,vector<int>(n));
+MATR genRandomGraph(int n, int m) {
+    MATR graph (n, vector<int>(n));
 
     for (int i = 0; i < n; i++) {
-        graph[n].resize(n, 0);
         graph[i][i] = -1;
     }
-    int m = rand() % ((n*(n-1))/2+1);
 
-    while (m > 0){
+    while (m > 0) {
         int i = rand() % n;
         int j = rand() % n;
-        if(i != j and graph[i][j] == 0){
-             graph[i][j] = 1;
-             graph[j][i] = 1;
-             --m;
+        if (i != j and graph[i][j] == 0) {
+            graph[i][j] = 1;
+            graph[j][i] = 1;
+            --m;
         }
     }
     return graph;
 }
 
-MATR genErdos(int n, int m){
-    assert (m > (n*(n-1))/2);
-    MATR graph (n,vector<int>(n));
+MATR genErdos(int n, int m) {
+    assert (m > (n * (n - 1)) / 2);
+    MATR graph (n, vector<int>(n));
 
     for (int i = 0; i < n; i++) {
         graph[n].resize(n, 0);
         graph[i][i] = -1;
     }
 
-    while (m > 0){
+    while (m > 0) {
         int i = rand() % n;
         int j = rand() % n;
-        if(i != j and graph[i][j] == 0){
-             graph[i][j] = 1;
-             graph[j][i] = 1;
-             --m;
+        if (i != j and graph[i][j] == 0) {
+            graph[i][j] = 1;
+            graph[j][i] = 1;
+            --m;
         }
     }
     return graph;
@@ -78,54 +75,54 @@ MATR genErdos(int n, int m){
 
 //Models:
 MATR gilbert (int n, float p) {
-	MATR result (n,vector<int>(n));
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			if (i != j and not seBorra((float)((float)1-(float)p))) {
-				result[i][j] = 1;
-				result[j][i] = 1;
-			}
-			else {
-				result[i][j] = 0;
-				result[j][i] = 0;
-			}
-		}
-	}
-	return result;
+    MATR result (n, vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i != j and not seBorra((float)((float)1 - (float)p))) {
+                result[i][j] = 1;
+                result[j][i] = 1;
+            }
+            else {
+                result[i][j] = 0;
+                result[j][i] = 0;
+            }
+        }
+    }
+    return result;
 }
 
 
 int calcul(int n, int m) {
-	int val = n*(n-1)/2;
-	int rest = val - m;
-	int parcial = 1;
-	while (val >= m) {
-		parcial *= val;
-		val = val-1;
-	}
-	while (rest >= 1) {
-		parcial /= rest;
-	}
-	return parcial;
+    int val = n * (n - 1) / 2;
+    int rest = val - m;
+    int parcial = 1;
+    while (val >= m) {
+        parcial *= val;
+        val = val - 1;
+    }
+    while (rest >= 1) {
+        parcial /= rest;
+    }
+    return parcial;
 }
 
 
 MATR erdos (int n, int m) {
-	MATR result (n,vector<int>(n));
-	double p = 1/calcul(n,m);
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			if (not seBorra(p)) {
-				result[i][j] = true;
-				result[j][i] = true;
-			}
-			else {
-				result[i][j] = false;
-				result[j][i] = false;
-			}
-		}
-	}
-  return result;
+    MATR result (n, vector<int>(n));
+    double p = 1 / calcul(n, m);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (not seBorra(p)) {
+                result[i][j] = true;
+                result[j][i] = true;
+            }
+            else {
+                result[i][j] = false;
+                result[j][i] = false;
+            }
+        }
+    }
+    return result;
 }
 
 
--- properties.cc
+++ properties.cc
@@ -7,80 +7,83 @@ using namespace std;
 bool testTopBottom(const MATR& matrix, float q, bool node, const vector<int>& top, const vector<int>& bottom) {
 	//Top Auxiliar=n, Bottom Auxiliar=n+1
 	int n = matrix.size();
-	unionFind u(n+2);
+	unionFind u(n + 2);
 	if (node) {
-		for (uint i = 0; i<matrix.size(); ++i) {
-			if(seBorra(q)) u.delfind(i);
+		for (uint i = 0; i < matrix.size(); ++i) {
+			if (seBorra(q)) u.delfind(i);
 		}
-		for (uint i = 0; i<matrix.size(); ++i) {
-			if (u.find(i)!=-1) {
-				for (uint j= i+1; j<matrix.size(); ++j) {
+		for (uint i = 0; i < matrix.size(); ++i) {
+			if (u.find(i) != -1) {
+				for (uint j = i + 1; j < matrix.size(); ++j) {
 					if (u.find(j) != -1 and matrix[i][j])
-						u.unir(i,j);
+						u.unir(i, j);
 				}
 			}
 		}
 		for (uint i = 0; i < top.size(); ++i) {
 			if (u.find(top[i]) != -1)
-				u.unir(n,top[i]);
+				u.unir(n, top[i]);
 		}
 		for (uint i = 0; i < bottom.size(); ++i) {
 			if (u.find(bottom[i]) != -1)
-				u.unir(n+1,bottom[i]);
+				u.unir(n + 1, bottom[i]);
 		}
 	}
 	else {
 		for (uint i = 0; i < matrix.size(); ++i) {
-			for (uint j = i+1; j < matrix.size(); ++j) {
+			for (uint j = i + 1; j < matrix.size(); ++j) {
 				if (matrix[i][j] == 1 and not seBorra(q)) {
-					u.unir(i,j);
+					u.unir(i, j);
 				}
 			}
 		}
 		for (uint i = 0; i < top.size(); ++i)
-			u.unir(n,top[i]);
+			u.unir(n, top[i]);
 		for (uint i = 0; i < bottom.size(); ++i)
-			u.unir(n+1,bottom[i]);
+			u.unir(n + 1, bottom[i]);
 	}
-	return (u.find(n) == u.find(n+1));
+	return (u.find(n) == u.find(n + 1));
 }
 
 void DFS(int i, uint numVertices, vector<bool>& visited, vector<bool>& even, const vector<bool>& erased, const MATR& graph)
 {
-    if (erased[i]) DFS(i+1, numVertices, visited, even, erased, graph);
-    else{
-      int connections = 0;
-      visited[i]=true;
-      for(uint j=0; j<numVertices; j++){
-        if(graph[i][j]==1 and !erased[j]){
-            connections++;
-            if (!visited[j]) {
-              DFS(j, numVertices, visited, even, erased, graph);
-            }
-        }
-      }
-      if (connections == 0) DFS(i+1, numVertices, visited, even, erased, graph);
-      else if (connections%2 == 0) even[i] = true;
-    }
+	if (erased[i]) DFS(i + 1, numVertices, visited, even, erased, graph);
+	else {
+		int connections = 0;
+		visited[i] = true;
+		for (uint j = 0; j < numVertices; j++) {
+			if (graph[i][j] == 1 and !erased[j]) {
+				connections++;
+				if (!visited[j]) {
+					DFS(j, numVertices, visited, even, erased, graph);
+				}
+			}
+		}
+		if (connections == 0) {
+			if (i + 1 < (int)numVertices)DFS(i + 1, numVertices, visited, even, erased, graph);
+			even[i] = true;
+		}
+		else if (connections % 2 == 0) even[i] = true;
+	}
 }
 
 //0: el graf no es euleria ni te un sendero euleria
 //1: el graf no es euleria pero si te un sendero euleria
 //2: el graf es euleria
-int testEulerian(const MATR& graph, const vector<bool>& erased){
-  vector<bool> even(graph.size(), false);
-  vector<bool> visited(graph.size(), false);
-  DFS(0, graph.size(), visited, even, erased, graph);
-  int odds = 0;
-  for(uint j=0; j<graph.size(); j++){
-    if(!visited[j] and !erased[j]){
-      for(int adj: graph[j]) if (adj == 1) return 0;
-    }
-    else{
-      if(!even[j] and !erased[j]) odds++;
-      if(odds > 2) return 0;
-    }
-  }
-  if(odds == 2) return 1;
-  return 2;
+int testEulerian(const MATR& graph, const vector<bool>& erased) {
+	vector<bool> even(graph.size(), false);
+	vector<bool> visited(graph.size(), false);
+	DFS(0, graph.size(), visited, even, erased, graph);
+	int odds = 0;
+	for (uint j = 0; j < graph.size(); j++) {
+		if (!visited[j] and !erased[j]) {
+			for (int adj : graph[j]) if (adj == 1) return 0;
+		}
+		else {
+			if (!even[j] and !erased[j]) odds++;
+			if (odds > 2) return 0;
+		}
+	}
+	if (odds == 2) return 1;
+	return 2;
 }
